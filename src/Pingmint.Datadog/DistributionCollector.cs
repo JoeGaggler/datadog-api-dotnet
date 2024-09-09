feb1ed86283cbd3458d6b97110e29ba01fa010d1
Joe Gallagher <joegaggler@gmail.com>
2024-09-09 15:34:11 -0400

    add distribution point collector

---
+++ src/Pingmint.Datadog/DistributionCollector.cs
@@ -0,0 +1,82 @@
+namespace Pingmint.Datadog;
+
+
+public class DistributionCollector
+{
+    private readonly Object lockObject = new();
+    private readonly List<String> sharedTags;
+    private List<DistributionSeries> series = new();
+
+    public DistributionCollector(List<String> sharedTags)
+    {
+        this.sharedTags = sharedTags;
+    }
+
+    public void Add(String metric, List<String> tags, DistributionPoint point)
+    {
+        lock (lockObject)
+        {
+            // This performs a linear search of the list of series when tags are used, but the list is expected to be small.
+            DistributionSeries? seriesToUpdate = null;
+            foreach (var candidateSeries in series)
+            {
+                if (candidateSeries.Metric != metric) { continue; }
+                if (candidateSeries.Tags is not { } candidateTags || candidateTags.Count == 0) { continue; }
+                if (!candidateTags.SequenceEqual(tags)) { continue; }
+
+                seriesToUpdate = candidateSeries;
+                break;
+            }
+
+            if (seriesToUpdate is null)
+            {
+                seriesToUpdate = new()
+                {
+                    Metric = metric,
+                    Points = [],
+                    Tags = tags,
+                };
+                series.Add(seriesToUpdate);
+            }
+
+            seriesToUpdate.Points!.Add(point);
+        }
+    }
+
+    public DistributionRequest? GetRequest()
+    {
+        List<DistributionSeries> distToSubmit;
+        lock (lockObject)
+        {
+            distToSubmit = series;
+            series = [];
+        }
+
+        if (distToSubmit.Count == 0)
+        {
+            return null;
+        }
+
+        // Add shared tags and resources
+        foreach (var series in distToSubmit)
+        {
+            if (series.Tags is null)
+            {
+                series.Tags = sharedTags;
+            }
+            else
+            {
+                series.Tags.AddRange(sharedTags);
+            }
+
+            // Note: resources are not yet supported for distributions
+            // https://docs.datadoghq.com/api/latest/metrics/#submit-distribution-points
+        }
+
+        var model = new Pingmint.Datadog.DistributionRequest()
+        {
+            Series = distToSubmit
+        };
+        return model;
+    }
+}
--- src/Pingmint.Datadog/ModelFactory.cs
+++ src/Pingmint.Datadog/ModelFactory.cs
@@ -2,9 +2,15 @@ namespace Pingmint.Datadog;
 
 public static class ModelFactory
 {
-    public static Point CurrentPoint(decimal value) => new()
+    public static Point CurrentPoint(Decimal value) => new()
     {
         Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
         Value = value
+    };
+
+    public static DistributionPoint CurrentDistribution(List<Decimal> values) => new()
+    {
+        Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+        Value = new(values),
     };
 }
