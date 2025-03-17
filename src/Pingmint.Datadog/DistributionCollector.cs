1b535efee8792a7da34ff0cc0ec35f1dd430cd1b
Joe Gallagher <joegaggler@gmail.com>
2025-03-17 15:58:07 -0400

    add lock to distribution collector

--- src/Pingmint.Datadog/DistributionCollector.cs
+++ src/Pingmint.Datadog/DistributionCollector.cs
@@ -1,6 +1,5 @@
 namespace Pingmint.Datadog;
 
-
 public class DistributionCollector
 {
     private readonly Object lockObject = new();
@@ -14,63 +13,69 @@ public class DistributionCollector
 
     public void Add(String metric, List<String> tags, DistributionPoint point)
     {
-        // This performs a linear search of the list of series when tags are used, but the list is expected to be small.
-        DistributionSeries? seriesToUpdate = null;
-        foreach (var candidateSeries in series)
+        lock (lockObject)
         {
-            if (candidateSeries.Metric != metric) { continue; }
-            if (candidateSeries.Tags is not { } candidateTags || candidateTags.Count == 0) { continue; }
-            if (!candidateTags.SequenceEqual(tags)) { continue; }
-
-            seriesToUpdate = candidateSeries;
-            break;
-        }
-
-        if (seriesToUpdate is null)
-        {
-            seriesToUpdate = new()
+            // This performs a linear search of the list of series when tags are used, but the list is expected to be small.
+            DistributionSeries? seriesToUpdate = null;
+            foreach (var candidateSeries in series)
             {
-                Metric = metric,
-                Points = [],
-                Tags = tags,
-            };
-            series.Add(seriesToUpdate);
-        }
+                if (candidateSeries.Metric != metric) { continue; }
+                if (candidateSeries.Tags is not { } candidateTags || candidateTags.Count == 0) { continue; }
+                if (!candidateTags.SequenceEqual(tags)) { continue; }
 
-        seriesToUpdate.Points!.Add(point); // Points is always initialized to an empty list before adding to the series.
+                seriesToUpdate = candidateSeries;
+                break;
+            }
+
+            if (seriesToUpdate is null)
+            {
+                seriesToUpdate = new()
+                {
+                    Metric = metric,
+                    Points = [],
+                    Tags = tags,
+                };
+                series.Add(seriesToUpdate);
+            }
+
+            seriesToUpdate.Points!.Add(point); // Points is always initialized to an empty list before adding to the series.
+        }
     }
 
     public DistributionRequest? GetRequest()
     {
-        List<DistributionSeries> distToSubmit = series;
-        this.series = [];
-
-        if (distToSubmit.Count == 0)
+        lock (lockObject)
         {
-            return null;
-        }
+            List<DistributionSeries> distToSubmit = series;
+            this.series = [];
 
-        // Add shared tags and resources
-        foreach (var series in distToSubmit)
-        {
-            if (series.Tags is null)
+            if (distToSubmit.Count == 0)
             {
-                series.Tags = sharedTags;
-            }
-            else
-            {
-                series.Tags.AddRange(sharedTags);
+                return null;
             }
 
-            // Note: resources are not yet supported for distributions
-            // https://docs.datadoghq.com/api/latest/metrics/#submit-distribution-points
-        }
+            // Add shared tags and resources
+            foreach (var series in distToSubmit)
+            {
+                if (series.Tags is null)
+                {
+                    series.Tags = sharedTags;
+                }
+                else
+                {
+                    series.Tags.AddRange(sharedTags);
+                }
 
-        var model = new Pingmint.Datadog.DistributionRequest()
-        {
-            Series = distToSubmit
-        };
-        return model;
+                // Note: resources are not yet supported for distributions
+                // https://docs.datadoghq.com/api/latest/metrics/#submit-distribution-points
+            }
+
+            var model = new Pingmint.Datadog.DistributionRequest()
+            {
+                Series = distToSubmit
+            };
+            return model;
+        }
     }
 
     public IDisposable AddScopeDurationSeconds(String metric, List<String> tags)
